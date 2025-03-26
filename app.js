9931ae63e9a696ef599652938ec127a3c9655de7
Pema2004Namgay <07230019.sherubtse@rub.edu.bt>
2025-03-26 10:37:50 +0600

    Practical 3

--- app.js
+++ app.js
@@ -1,47 +1,55 @@
 // Import required modules
 const express = require('express');
 const path = require('path');
-const dotenv = require('dotenv')
+const dotenv  =  require('dotenv')
 const session = require('express-session');
+const db = require('./config/db');
 const bodyParser = require('body-parser');
+const indexRoutes = require('./routes/index');
+const authRoutes = require('./routes/authRoutes');
+const homeRoutes = require('./routes/homeRoutes');
+const profileRoutes = require('./routes/profileRoutes');
+
 
 //load environment variables
 dotenv.config();
 
+
 // Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'gyeltshenMk',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: 60 * 60 * 1000 //session expries after 1 hour
+    }
+}));
+
 
 // Set view engine to EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/components', express.static(path.join(__dirname, 'views/components')));
 
-//Middleware
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Session management
-app.use(session({
-    secret: 'your_secret_key', 
-    resave: false, 
-    saveUninitialized: true, 
-    cookie: { secure: false }
-}));
-
-// Import routes
-const indexRoutes = require('./routes/index');
-const authRoutes = require('./routes/authRoutes');
-const db = require('./config/db'); // Import database connection
-
-
-//Define routes
+// routes
 app.use('/', indexRoutes);
 app.use('/auth', authRoutes);
+app.use('/', homeRoutes);
+app.use('/', profileRoutes);
 
-// Database test route
+
+//for db connection
 app.get('/db-test', async (req, res) => {
     try {
         const result = await db.one('SELECT NOW() AS current_time');
@@ -51,15 +59,8 @@ app.get('/db-test', async (req, res) => {
     }
 });
 
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-
-
-
-
-
-
